7900657b1217e437da61b98de507381aaf783f8a
Gaia Koren <gaia.koren19@gmail.com>
2025-12-19 15:40:05 +0200

    create sum_between

---
+++ sum_numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+long long sum_between(const int from_number, const int to_number) {
+    return ((long long) from_number + to_number) * (to_number - from_number + 1) / 2;
+}
+
+int main(void) {
+    printf("%lld\n", sum_between(19, 112));
+    return 0;
+}
