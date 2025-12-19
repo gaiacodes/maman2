7d71664beaeb968c961a6d2b6ac66b30ccc8b596
Gaia Koren <gaia.koren19@gmail.com>
2025-12-19 16:31:27 +0200

    create SUM_BETWEEN macro

--- sum_numbers.c
+++ sum_numbers.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#define SUM_BETWEEN(from, to) \
+(((long long)(from) + (to)) * ((to) - (from) + 1) / 2)
 
 long long sum_between(const int from_number, const int to_number) {
     return ((long long) from_number + to_number) * (to_number - from_number + 1) / 2;
 }
 
 int main(void) {
-    printf("%lld\n", sum_between(19, 112));
+    printf("%lld\n", sum_between(-19, 112));
     return 0;
 }
